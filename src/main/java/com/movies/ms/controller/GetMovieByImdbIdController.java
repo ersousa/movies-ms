782de2a47c4dec636c72a211ef62134a9fdc1657
marciofiladelfo <marcio.kabana@gmail.com>
2023-03-24 22:11:21 -0300

    Implementado chamada com imdbId

--- src/main/java/com/movies/ms/controller/GetMovieByImdbIdController.java
+++ src/main/java/com/movies/ms/controller/GetMovieByImdbIdController.java
@@ -1,9 +1,23 @@
 package com.movies.ms.controller;
 
+import com.movies.ms.model.MovieEntity;
+import com.movies.ms.service.GetMovieByImdbIdService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/movies")
+@RequiredArgsConstructor
 public class GetMovieByImdbIdController {
+
+    private final GetMovieByImdbIdService service;
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<MovieEntity> getById(@PathVariable("id") final String id) {
+        return service.get(id);
+    }
 }
--- src/main/java/com/movies/ms/service/GetMovieByImdbIdService.java
+++ src/main/java/com/movies/ms/service/GetMovieByImdbIdService.java
@@ -1,4 +1,27 @@
 package com.movies.ms.service;
 
+import com.movies.ms.model.MovieEntity;
+import com.movies.ms.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class GetMovieByImdbIdService {
+
+    private final MovieRepository movieRepository;
+
+    public ResponseEntity<MovieEntity> get(String id) {
+
+        var optional = movieRepository.findByImdbIdIgnoreCase(id);
+        if (optional.isEmpty()) {
+            return ResponseEntity
+                    .notFound()
+                    .build();
+        }
+        return ResponseEntity
+                .ok(optional.get());
+
+    }
 }
