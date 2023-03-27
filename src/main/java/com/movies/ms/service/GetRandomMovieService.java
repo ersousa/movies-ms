f34e3c518c2c9e775fa17ec9ac74b52c095dbed1
marciofiladelfo <marcio.kabana@gmail.com>
2023-03-27 19:42:06 -0300

    Implementado chamada com Random

--- src/main/java/com/movies/ms/service/GetRandomMovieService.java
+++ src/main/java/com/movies/ms/service/GetRandomMovieService.java
@@ -1,4 +1,22 @@
 package com.movies.ms.service;
 
+import com.movies.ms.model.MovieEntity;
+import com.movies.ms.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class GetRandomMovieService {
+
+    private final MovieRepository movieRepository;
+
+    public Optional<MovieEntity> listRandomMovies() {
+        final long count = movieRepository.count();
+        final int index = (int) (Math.random() * count);
+        return movieRepository.findAll(PageRequest.of(index, 1)).stream().findFirst();
+    }
 }
